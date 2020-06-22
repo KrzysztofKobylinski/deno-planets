7bc5cb903db4ea270c4a940d930578bc134e9288
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-06-22 18:25:37 +0200

    reading and parsing csv file

--- mod.ts
+++ mod.ts
@@ -1,9 +1,17 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
 
-async function readFile() {
-  const path = join("text_files", "input.txt")
-  const data = await Deno.readTextFile(path);
-  console.log(data);
+async function loadPlanetsData() {
+  const path = join(".", "planets.csv")
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+  const result = await parse(bufReader, {
+    header: true,
+    comment: '#',
+  })
+  console.log(result);
+  Deno.close(file.rid)
 }
 
-await readFile();
+await loadPlanetsData();
