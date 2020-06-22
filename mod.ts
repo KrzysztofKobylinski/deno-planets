fef8d7f820d0d996a5dc7b925fe37814f43ce181
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-06-22 18:45:05 +0200

    habitable planets filtering

--- mod.ts
+++ mod.ts
@@ -2,6 +2,9 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+interface Planet {
+  [ key: string ]: string;
+}
 async function loadPlanetsData() {
   const path = join(".", "planets.csv")
   const file = await Deno.open(path);
@@ -10,8 +13,19 @@ async function loadPlanetsData() {
     header: true,
     comment: '#',
   })
-  console.log(result);
   Deno.close(file.rid)
+  const planets = (result as Planet[]).filter((planet) => {
+    const prad = Number(planet["koi_prad"])
+    const smass = Number(planet['koi_smass'])
+    const srad = Number(planet["koi_srad"])
+    const confirmedDisposition = planet["koi_disposition"] === 'CONFIRMED';
+    const similarRadious = prad > 0.5 && prad < 1.5;
+    const similarMass = smass > 0.78 && smass < 1.04;
+    const similarStellarRadious = srad > 0.99 && srad < 1.01;
+    return confirmedDisposition && similarRadious && similarMass && similarStellarRadious;
+  })
+  return planets;
 }
 
-await loadPlanetsData();
+const planets = await loadPlanetsData();
+console.log(`${planets.length} - amount of planets`);
