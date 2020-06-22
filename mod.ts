a83e0ceff3326a0dcf43d75fff169209b218f888
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-06-22 18:12:43 +0200

    use of deno path module

--- mod.ts
+++ mod.ts
@@ -1,5 +1,8 @@
+import { join } from "https://deno.land/std/path/mod.ts";
+
 async function readFile() {
-  const data = await Deno.readTextFile("input.txt");
+  const path = join("text_files", "input.txt")
+  const data = await Deno.readTextFile(path);
   console.log(data);
 }
 
